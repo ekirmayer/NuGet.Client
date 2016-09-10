5093c6a7bd8978345dccf840a35484343c0b4e1c
Justin Emgarten <justin@emgarten.com>
2016-09-10 15:49:09 -0700

    Adding spec validation

---
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/Utility/SpecValidationUtility.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using NuGet.LibraryModel;
+using NuGet.ProjectModel;
+
+namespace NuGet.Commands
+{
+    public static class SpecValidationUtility
+    {
+        public static void ValidateDependencySpec(DependencyGraphSpec spec)
+        {
+            if (spec == null)
+            {
+                throw new ArgumentNullException(nameof(spec));
+            }
+
+            // Verify projects
+            foreach (var projectSpec in spec.Projects)
+            {
+                ValidateProjectSpec(projectSpec);
+            }
+
+            var restoreSet = new HashSet<string>(spec.Restore, StringComparer.Ordinal);
+            var projectSet = new HashSet<string>(
+                spec.Projects.Select(p => p.MSBuildMetadata?.ProjectUniqueName)
+                .Where(s => !string.IsNullOrEmpty(s)),
+                StringComparer.Ordinal);
+
+            // Verify restore does not reference a project that does not exist
+            foreach (var missing in restoreSet.Except(projectSet))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.SpecValidationMissingProject,
+                    missing);
+
+                throw RestoreSpecException.Create(message, Enumerable.Empty<string>());
+            }
+
+            // Verify restore contains at least one project
+            if (restoreSet.Count < 1)
+            {
+                throw RestoreSpecException.Create(Strings.SpecValidationZeroRestoreRequests, Enumerable.Empty<string>());
+            }
+        }
+
+        public static void ValidateProjectSpec(PackageSpec spec)
+        {
+            if (spec == null)
+            {
+                throw new ArgumentNullException(nameof(spec));
+            }
+
+            // Track the spec path
+            var files = new Stack<string>();
+            files.Push(spec.FilePath);
+
+            // restore metadata must exist for all project types
+            var restoreMetadata = spec.MSBuildMetadata;
+
+            if (restoreMetadata == null)
+            {
+                var message = string.Format(CultureInfo.CurrentCulture, Strings.MissingRequiredProperty, nameof(spec.MSBuildMetadata));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            // Track the project path
+            files.Push(restoreMetadata.ProjectPath);
+
+            var outputType = spec.MSBuildMetadata?.OutputType;
+
+            // Verify frameworks
+            ValidateFrameworks(spec, files);
+
+            // Verify project references
+            ValidateProjectReferences(spec, files);
+
+            // Verify based on the type.
+            switch (outputType)
+            {
+                case RestoreOutputType.NETCore:
+                    ValidateProjectSpecNetCore(spec, files);
+                    break;
+                case RestoreOutputType.UAP:
+                    ValidateProjectSpecUAP(spec, files);
+                    break;
+                default:
+                    ValidateProjectSpecOther(spec, files);
+                    break;
+            }
+        }
+
+        private static void ValidateFrameworks(PackageSpec spec, IEnumerable<string> files)
+        {
+            var frameworks = spec.TargetFrameworks.Select(f => f.FrameworkName).ToArray();
+
+            // Verify frameworks are valid
+            foreach (var framework in frameworks.Where(f => !f.IsSpecificFramework))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.Error_InvalidTargetFramework,
+                    framework.GetShortFolderName());
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            // Must have at least 1 framework
+            if (frameworks.Length < 1)
+            {
+                throw RestoreSpecException.Create(Strings.SpecValidationNoFrameworks, files);
+            }
+
+            // Duplicate frameworks may not exist
+            if (frameworks.Length != frameworks.Distinct().Count())
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.SpecValidationDuplicateFrameworks,
+                    string.Join(", ", frameworks.Select(f => f.GetShortFolderName())));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+        }
+
+        private static void ValidateProjectSpecNetCore(PackageSpec spec, IEnumerable<string> files)
+        {
+            // NETCore may not specify a project.json file
+            if (!string.IsNullOrEmpty(spec.MSBuildMetadata.ProjectJsonPath))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.PropertyNotAllowedForProjectType,
+                    RestoreOutputType.NETCore.ToString(),
+                    nameof(spec.MSBuildMetadata.ProjectJsonPath));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            // Output path must be set for netcore
+            if (string.IsNullOrEmpty(spec.MSBuildMetadata.OutputPath))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.MissingRequiredPropertyForProjectType,
+                    RestoreOutputType.NETCore.ToString(),
+                    nameof(spec.MSBuildMetadata.OutputPath));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+        }
+
+        private static void ValidateProjectSpecUAP(PackageSpec spec, IEnumerable<string> files)
+        {
+            // UAP may contain only 1 framework
+            if (spec.TargetFrameworks.Count != 1)
+            {
+                throw RestoreSpecException.Create(Strings.SpecValidationUAPSingleFramework, files);
+            }
+
+            // UAP must specify a project.json file
+            if (string.IsNullOrEmpty(spec.MSBuildMetadata.ProjectJsonPath)
+                || spec.MSBuildMetadata.ProjectJsonPath != spec.FilePath)
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.MissingRequiredPropertyForProjectType,
+                    RestoreOutputType.UAP.ToString(),
+                    nameof(spec.MSBuildMetadata.ProjectJsonPath));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            // Do not allow changing the output path for UAP
+            if (!string.IsNullOrEmpty(spec.MSBuildMetadata.OutputPath))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.PropertyNotAllowedForProjectType,
+                    RestoreOutputType.UAP.ToString(),
+                    nameof(spec.MSBuildMetadata.OutputPath));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+        }
+
+        private static void ValidateProjectSpecOther(PackageSpec spec, IEnumerable<string> files)
+        {
+            // Unknown project types may not have a project.json path
+            if (spec.MSBuildMetadata.ProjectJsonPath != null)
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.PropertyNotAllowed,
+                    nameof(spec.MSBuildMetadata.ProjectJsonPath));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            // Unknown project types may not carry package dependencies
+            var packageDependencies = GetAllDependencies(spec)
+                .Where(d => d.LibraryRange.TypeConstraintAllows(LibraryDependencyTarget.Package));
+
+            if (packageDependencies.Any())
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.PropertyNotAllowed,
+                    nameof(spec.Dependencies));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+        }
+
+        private static void ValidateProjectReferences(PackageSpec spec, IEnumerable<string> files)
+        {
+            var dependencies = new HashSet<string>(GetAllDependencies(spec)
+                .Where(d => d.LibraryRange.TypeConstraintAllows(LibraryDependencyTarget.ExternalProject))
+                .Select(d => d.Name),
+                StringComparer.OrdinalIgnoreCase);
+
+            var projectOnly = new HashSet<string>(GetAllDependencies(spec)
+                .Where(d => d.LibraryRange.TypeConstraintAllows(LibraryDependencyTarget.ExternalProject)
+                        && !d.LibraryRange.TypeConstraintAllows(LibraryDependencyTarget.Package)
+                        && !d.LibraryRange.TypeConstraintAllows(LibraryDependencyTarget.Reference))
+                .Select(d => d.Name),
+                StringComparer.OrdinalIgnoreCase);
+
+            var externalReferences = new HashSet<string>(
+                spec.MSBuildMetadata.ProjectReferences.Select(p => p.ProjectUniqueName),
+                StringComparer.OrdinalIgnoreCase);
+
+            foreach (var missing in externalReferences.Except(dependencies))
+            {
+                // Missing dependency in dependencies section
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.SpecValidationMissingDependency,
+                    missing);
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            foreach (var missing in projectOnly.Except(dependencies))
+            {
+                // missing restore section reference containing project path
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.SpecValidationMissingDependency,
+                    missing);
+
+                throw RestoreSpecException.Create(message, files);
+            }
+        }
+
+        private static void ValidateProjectMetadata(PackageSpec spec, IEnumerable<string> files)
+        {
+            // spec file path must be set
+            if (string.IsNullOrEmpty(spec.FilePath))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.MissingRequiredProperty,
+                    nameof(spec.FilePath));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            // spec name must be set
+            if (string.IsNullOrEmpty(spec.Name))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.MissingRequiredProperty,
+                    nameof(spec.Name));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            // unique name must be set
+            if (string.IsNullOrEmpty(spec.MSBuildMetadata.ProjectUniqueName))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.MissingRequiredProperty,
+                    nameof(spec.MSBuildMetadata.ProjectUniqueName));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            // project name must be set
+            if (string.IsNullOrEmpty(spec.MSBuildMetadata.ProjectName))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.MissingRequiredProperty,
+                    nameof(spec.MSBuildMetadata.ProjectName));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+
+            // msbuild project path must be set
+            if (string.IsNullOrEmpty(spec.MSBuildMetadata.ProjectPath))
+            {
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    Strings.MissingRequiredProperty,
+                    nameof(spec.MSBuildMetadata.ProjectPath));
+
+                throw RestoreSpecException.Create(message, files);
+            }
+        }
+
+        private static IEnumerable<LibraryDependency> GetAllDependencies(PackageSpec spec)
+        {
+            return spec.Dependencies
+                .Concat(spec.TargetFrameworks.SelectMany(f => f.Dependencies));
+        }
+    }
+}
